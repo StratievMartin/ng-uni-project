d3bb0afa9808918fa2987b3cb6c02ffad83e374c
Martin <martinstratiev1@gmail.com>
2022-02-25 00:24:08 +0200

    added auth components

---
+++ src/app/ad.model.ts
@@ -0,0 +1,10 @@
+export class Ad{
+  id!: string;
+  heading!: string;
+  description!: string;
+  likes!: string;
+  type!: string;
+  category!: string;
+}
+// can have only 1 category
+// should have a list of all candidates for the current ad and if they are accepted or not 
